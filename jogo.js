488fead54724be3f62a7790fefb4315ee12d3dde
Wictor Ducla <wictor.ducla14@gmail.com>
2023-05-22 15:54:22 -0300

    Criando posição aleatoria e ajuste de tamanho para o mosquito

--- jogo.js
+++ jogo.js
@@ -4,6 +4,40 @@ function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
 
+    console.log(largura, altura);
 }
 
+ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica(){
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+    
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY);
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'img/mosca.png'
+    mosquito.className = tamanhoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
